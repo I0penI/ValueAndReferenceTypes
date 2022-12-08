6be0c9a2792c806f9e7e2364de9e2bc275ef0a54
ali.arslan <ali.arslan@ANK5-YZLMORT-10>
2022-12-08 11:28:03 +0300

    Add project files.

---
+++ TryParseVbOrnekler/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace TryParseVbOrnekler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sayiText = "27";
+            //int sayi = int.Parse(sayiText);
+            //Console.WriteLine(sayi);
+
+            int donusturulenSayi;
+            bool sonuc= int.TryParse(sayiText, out donusturulenSayi);
+            if (sonuc)
+            {
+                Console.WriteLine("Sayı Dönüştürüldü (" + donusturulenSayi + ")");
+            }
+            else
+            {
+                Console.WriteLine("Sayı Dönüştürülemedi.");
+            }
+
+            double carpim;
+            double toplam = ToplaVeCarp(1.2, 4.6, out carpim);
+            Console.WriteLine($"Toplam: {toplam}, Çarpım: {carpim}");
+            float bolum = 0;
+            float fark = CikarVeBol(2.4f, 2, ref bolum);
+            Console.WriteLine($"Fark: {fark}, Bölüm: {bolum}");
+
+        }
+
+        static double ToplaVeCarp(double sayi1, double sayi2, out double carpim)
+        {
+            carpim = sayi1 * sayi2;
+            return sayi1 + sayi2;
+        }
+        static float CikarVeBol(float sayi1, float sayi2, ref float bolum)
+        {
+            bolum = sayi1 / sayi2;
+            return sayi1 - sayi2;
+        }
+    }
+}
---
+++ ValueAndReferenceTypes/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace ValueAndReferenceTypes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int id = 1;
+            IdGuncelle(id);
+            Console.WriteLine(id);
+
+            Ogrenci ogrenci = new Ogrenci() // Referans tip
+            {
+                Id = 1
+            };
+            IdGuncelle(ogrenci);
+            
+                Console.WriteLine(ogrenci.Id);
+            
+        }
+
+        static void IdGuncelle(int id)
+        {
+            id = 7;
+        }
+        static void IdGuncelle(Ogrenci ogrenci)
+        {
+            ogrenci.Id = 7;
+        }
+    }
+    class Ogrenci
+    {
+        public int Id { get; set; }
+    }
+}
